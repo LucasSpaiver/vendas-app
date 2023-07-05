b836c745eb919c23dccc01693929b42cbde09efb
LucasSpavier <lucaspavier@gmail.com>
2023-07-05 12:15:37 -0300

    feat: create app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { Provider } from 'react-redux';
-
-import Navigation from './Navigation';
-import GlobalModal from './shared/components/modal/globalModal/GlobalModal';
-import store from './store';
+import { SafeAreaView, Text  } from "react-native";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <GlobalModal />
-      <Navigation />
-    </Provider>
-  );
+    <SafeAreaView>
+      <Text>Testando</Text>
+    </SafeAreaView>
+  )
 };
 
 export default App;
